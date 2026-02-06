d8fcf3cc73f2f8e074150031b6cd09290d3c0c30
Daniel Vila <codedany789@gmail.com>
2026-02-06 19:10:19 -0400

    Initialize tasky project with backend and frontend structure, including Maven setup and React configuration. Added essential files such as README, .gitignore, and configuration files for both backend (Spring Boot) and frontend (Vite with React). Implemented basic application structure and sample components for both parts.

---
+++ tasky-backend/src/main/java/com/tasky/app/AppApplication.java
@@ -0,0 +1,13 @@
+package com.tasky.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppApplication.class, args);
+	}
+
+}
